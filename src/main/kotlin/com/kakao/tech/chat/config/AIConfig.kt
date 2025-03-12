b79865bd03836bcf7f00e134b3a29a2ed5b072a3
robin-hwang <robin.hwang@kakaocorp.com>
2025-03-13 08:58:51 +0900

    Modify ChatClient Bean

--- src/main/kotlin/com/kakao/tech/chat/config/AIConfig.kt
+++ src/main/kotlin/com/kakao/tech/chat/config/AIConfig.kt
@@ -1,7 +1,6 @@
 package com.kakao.tech.chat.config
 
 import org.springframework.ai.chat.client.ChatClient
-import org.springframework.ai.chat.prompt.ChatOptions
 import org.springframework.ai.openai.OpenAiChatModel
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,14 +9,6 @@ import org.springframework.context.annotation.Configuration
 class AIConfig {
     @Bean
     fun openAIChatClient(chatModel: OpenAiChatModel): ChatClient {
-        return ChatClient.builder(chatModel)
-            .defaultOptions(
-                ChatOptions.builder()
-                    .model("gpt-4o")
-                    .temperature(0.0)
-                    .build()
-            )
-
-            .build()
+        return ChatClient.create(chatModel)
     }
 }
